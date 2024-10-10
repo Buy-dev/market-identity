e4577a7c9a79787e5654975603113a21db169f79
dvinogradov@ad.salyk.kg <dvinogradov@it.salyk.kg>
2024-10-10 18:39:19 +0600

    working on grpc

--- Market.Identity.Api/Services/AuthService.cs
+++ Market.Identity.Api/Services/AuthService.cs
@@ -1,8 +1,23 @@
+using Grpc.Core;
 using Market.Identity.Grpc;
+using MediatR;
 
 namespace Market.Identity.Services;
 
-public class AuthService : AuthGrpc.AuthGrpcBase
+public class AuthService(IMediator mediator) : AuthGrpc.AuthGrpcBase
 {
+    public override Task<LoginResponse> Login(LoginUserRequest request, ServerCallContext context)
+    {
+        return base.Login(request, context);
+    }
     
+    public override Task<RegisterResponse> Register(RegisterUserRequest request, ServerCallContext context)
+    {
+        return base.Register(request, context);
+    }
+    
+    public override Task<RefreshTokenResponse> RefreshToken(RefreshTokenRequest request, ServerCallContext context)
+    {
+        return base.RefreshToken(request, context);
+    }
 }
